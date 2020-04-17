369d6289e35975d6d515317e86321ecf378a9d58
digicope <51534708+digicope@users.noreply.github.com>
2020-04-17 16:54:32 +0900

    Add files via upload

---
+++ 파이썬프로그래밍/calculator.py
@@ -0,0 +1,26 @@
+def add(a,b):
+    c = a + b
+    return c
+
+def subtract(a,b):
+    c = a - b
+    return c
+
+def multiply(a,b):
+    c = a * b
+    return c
+
+def divide(a,b):
+    c = a / b
+    return c
+
+# print('나는 모듈입니다')
+
+if __name__ == '__main__':
+    print(add(10,20))
+    print(subtract(30,10))
+
+
+
+
+
