cfebbb078809f3e19febae9f2e9d1d466866a02e
digicope <51534708+digicope@users.noreply.github.com>
2020-04-20 09:57:04 +0900

    Add files via upload

--- 파이썬프로그래밍/calculator.py
+++ 파이썬프로그래밍/calculator.py
@@ -1,24 +1,40 @@
-def add(a,b):
-    c = a + b
-    return c
+class Calc:
+    def __init__(self) : # 생성자 메서드, 인스턴스 객체를 생성할 때 자동으로 호출, 인스턴스 멤버 초기화
+        print('생성자가 호출됨')
+        self.a = 0
+        self.b = 0
+        self.c = 0
+    
+    def add(self,a,b):
+        self.a = a
+        self.b = b
+        self.c = a + b
+        return self.c
 
-def subtract(a,b):
-    c = a - b
-    return c
+    def subtract(self,a,b):
+        self.a = a
+        self.b = b
+        self.c = a - b
+        return self.c
 
-def multiply(a,b):
-    c = a * b
-    return c
+    def multiply(self,a,b):
+        self.a = a
+        self.b = b
+        self.c = a * b
+        return self.c
 
-def divide(a,b):
-    c = a / b
-    return c
+    def divide(self,a,b):
+        self.a = a
+        self.b = b
+        self.c = a / b
+        return self.c
 
 # print('나는 모듈입니다')
 
 if __name__ == '__main__':
-    print(add(10,20))
-    print(subtract(30,10))
+    m1 = Calc()
+    print(m1.add(10,20))
+    print(m1.subtract(30,10))
 
 
 
